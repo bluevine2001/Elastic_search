9c9ab4fee82645ce91f312d273e46a3222c51fcd
oussama <oussamabechichi@gmail.com>
2023-05-24 10:52:44 +0200

    added tp1 answers + elk routes

--- README.md
+++ README.md
@@ -45,3 +45,56 @@ docker run --name kib-01 --net elastic -p 5601:5601 docker.elastic.co/kibana/kib
 Etape 9 : Copier le lien dans le terminal et l'ouvrir dans le navigateur
 
 Etape 10 : Se connecter avec les login elastic search
+
+TP-1 Elasticsearch
+
+Procédez à l’installation d’ElasticSearch et Kibana et Logstash sur votre machine :
+
+- [https://www.elastic.co/guide/en/elasticsearch/reference/current/install-elasticsearch.html](https://www.elastic.co/guide/en/elasticsearch/reference/current/install-elasticsearch.html)
+- Documentez toutes les étapes sur un dépôt Git
+
+Procédez à l’indexation des documents présenté en Cours.
+
+●Créer un index
+
+○Avec mapping explicite
+
+●Indexer des documents
+
+●Indexer des documents en lots (bulk)
+
+●Rechercher dans les documents
+
+○Via Kibana
+
+○Via API REST
+
+●Modifier la configuration d’un index
+
+Création d’index : [https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html](https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html)
+
+Depuis la plateforme Kaggle, récupérez et indexez un jeu de données conséquent qui servira de base à votre projet sur la semaine.
+
+Exercices sur le mapping :
+
+- Comment Elasticsearch procède-t-il au mapping ?
+  il se base sur le premier document pour créer automatiquement le mapping de l'index, mais l'utilisateur peut suggérer le maping de l'index en insérant le document.
+- Modifier un index pour affecter un mapping explicite
+- Peut-on modifier le mapping sans recréer l’index ?
+  non, il faut créer un index en précisant le mapping puis copier les données de l'ancien index vers le nouvel index.
+
+Exercices sur l’analyseur :
+
+- [https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis.html](https://www.elastic.co/guide/en/elasticsearch/reference/current/analysis.html)
+- Tentez de définir : Tokenisation et Normalisation 
+  La Tokenisation permet de découper une phrase en token (mots), ce qui permet une recherche plus vaste qu'une recherche littérale sur toute la chaine, chaque mot (token) est recherché afin d'avoir des matchs par tokens et non des matchs qui contiennent mot pour mot la phrase recherché.
+  Cependant la tokenisation permet uniquement la recherche litérale de chaque mot mais ne permet pas la recherche de mot légèrement différent mais pertinant,La Normalisation permet cela, en modifiant les mots dans le but de les normaliser, par exemple, un mot contenant une majuscule sera mis en minuscule, un mot au pluriel sera mis au singulier, et un synonyme sera indexé par un synonyme plus commun.
+
+Les APIs:
+
+Lors de la démonstration nous avons évoqué la notion d’API, desquelles avons-nous parlé ?
+
+Nous avons parler des APIs de Document (CRUD), qui permettent d'indexer un document, d'indexer des documents en masse (bulk), supprimer un document, mettre à jour un document,
+Nous avons aussi parler des APIs d'index, qui permettent de mettre à jour un index, créer un index, afficher le mapping d'un index,...
+
+[https://www.elastic.co/guide/en/elasticsearch/reference/current/rest-apis.html](https://www.elastic.co/guide/en/elasticsearch/reference/current/rest-apis.html)
---
+++ index.js
@@ -0,0 +1,174 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+require("dotenv").config();
+const fs = require("fs");
+
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({
+  node: "https://localhost:9200",
+  auth: {
+    username: "elastic",
+    password: process.env.ELASTIC_USER_PWD,
+  },
+  tls: {
+    ca: fs.readFileSync("./http_ca.crt"),
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/", (req, res) => {
+  // get all titles
+  /* client
+    .search({
+      index: "michelin",
+      query: {
+        match_all: {},
+      },
+    })
+    .then((result) => {
+      res.send(result.hits.hits);
+    })
+    .catch((err) => {
+      res.send(err);
+    }); */
+  // get a specific title
+  /*  client
+    .get({
+      index: "titles",
+      id: "3",
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    }); */
+  // update a specific title
+  /* client
+    .update({
+      index: "titles",
+      id: "3",
+      body: {
+        doc: {
+          title: "updated title",
+        },
+      },
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    }); */
+  // delete a specific title
+  /* client
+    .delete({
+      index: "titles",
+      id: "3",
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    }); */
+  //create index with mapping
+  /* client.indices
+    .create({
+      index: "ryadh-cafes",
+      body: {
+        mappings: {
+          properties: {
+            index: { type: "integer" },
+            coffeeName: { type: "text" },
+            rating: { type: "integer" },
+            rating_count: { type: "integer" },
+            URL: { type: "text" },
+            hours: { type: "boolean" },
+            location: { type: "geo_point" },
+          },
+        },
+      },
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    }); */
+
+  // add data to index bulk
+  client
+    .bulk({
+      index: "ryadh-cafes",
+      body: [
+        { index: { _index: "ryadh-cafes", _id: 1 } },
+        {
+          coffeeName: "Cacti Cafe",
+          rating: "4.1",
+          rating_count: "2212",
+          URL: "https://www.google.com/maps/place/Cacti+Cafe/data=!4m7!3m6!1s0x3e2efde0d2059f1d:0xfca400b51ca140cc!8m2!3d24.8339855!4d46.7356133!16s%2Fg%2F11mwzvn0zx!19sChIJHZ8F0uD9Lj4RzEChHLUApPw?authuser=0&hl=en&rclk=1",
+          hours: true,
+          location: { type: "Point", coordinates: [46.7356133, 24.8339855] },
+        },
+        { index: { _index: "ryadh-cafes", _id: 2 } },
+        {
+          coffeeName: "Fc Lounge",
+          rating: "3.5",
+          rating_count: "539",
+          URL: "https://www.google.com/maps/place/Fc+Lounge+-+%D8%A7%D9%81+%D8%B3%D9%8A+%D9%84%D8%A7%D9%88%D9%86%D8%AC%E2%80%AD/data=!4m7!3m6!1s0x3e2eff79d7dd770d:0xbd5029a7e12e0c2b!8m2!3d24.8131149!4d46.7680585!16s%2Fg%2F11lgkw1ffk!19sChIJDXfd13n_Lj4RKwwu4acpUL0?authuser=0&hl=en&rclk=1",
+          hours: true,
+          location: { type: "Point", coordinates: [46.7680585, 24.8131149] },
+        },
+        { index: { _index: "ryadh-cafes", _id: 3 } },
+        {
+          coffeeName: "PEAKS",
+          rating: "4.6",
+          rating_count: "25",
+          URL: "https://www.google.com/maps/place/PEAKS/data=!4m7!3m6!1s0x3e2ee380585f0151:0xab784cd32a1e3d85!8m2!3d24.742045!4d46.6348092!16s%2Fg%2F11s0qh89n0!19sChIJUQFfWIDjLj4RhT0eKtNMeKs?authuser=0&hl=en&rclk=1",
+          hours: false,
+          location: { type: "Point", coordinates: [46.6348092, 24.742045] },
+        },
+        { index: { _index: "ryadh-cafes", _id: 4 } },
+        {
+          coffeeName: "Joffrey's",
+          rating: "2.9",
+          rating_count: "34",
+          URL: "https://www.google.com/maps/place/Joffrey%27s+%D8%AC%D9%88%D9%81%D8%B1%D9%8A%D8%B2%E2%80%AD/data=!4m7!3m6!1s0x3e2f1d97addc7711:0xc93e4546dea3a486!8m2!3d24.7146875!4d46.6349375!16s%2Fg%2F11p00bzdsv!19sChIJEXfcrZcdLz4RhqSj3kZFPsk?authuser=0&hl=en&rclk=1",
+          hours: false,
+          location: { type: "Point", coordinates: [46.6349375, 24.7146875] },
+        },
+      ],
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.post("/search", (req, res) => {
+  client
+    .search({
+      index: "ryadh-caffees-test",
+      body: {
+        query: {
+          match_phrase: {
+            coffeeName: req.body.coffeeName,
+          },
+        },
+      },
+    })
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}...`);
+});
